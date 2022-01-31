c6683dac0dc3108509530e1295c0eb78e0ef4155
Christian-Mogensen <U2407192@rts-365.dk>
2022-01-31 17:19:48 +0100

    Add starter proper profil import comp

--- demo/light-app/src/components/header/Header.js
+++ demo/light-app/src/components/header/Header.js
@@ -4,7 +4,7 @@ import ProfilUser from '../../assets/img/profile/randomdasd213tasd.jpg'
 const Header = () => {
   return (<header className='flex justify-between items-center px-6 pt-8 w-[375px] fixed left-0 top-0'>
       <h1 className=' text-3xl font-bold text-white max-w-[10ch]'>Control Panel</h1>
-      <div className="relative"><figure className=' h-14 w-14 rounded-full border-2 overflow-hidden profil__img-dropshadow'><img className='h-14 object-cover' src={ProfilUser} alt="" /></figure><div className="absolute bg-[#FF9898] h-3 w-3 top-0 right-0 rounded-full"></div></div>
+  <ProfileImage src={ProfilUser}/>
   </header>);
 };
 
--- demo/light-app/src/components/header/ProfileImage.js
+++ demo/light-app/src/components/header/ProfileImage.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-const ProfileImage = ({img}) => {
-  return <img src={img} alt="" />;
+const ProfileImage = ({src}) => {
+  return <div className="relative"><figure className='overflow-hidden border-2 rounded-full  h-14 w-14 profil__img-dropshadow'><img className='object-cover h-14' src={src} alt="" /></figure><div className="absolute bg-[#FF9898] h-3 w-3 top-0 right-0 rounded-full"></div></div>
+  ;
 };
 
 export default ProfileImage;
