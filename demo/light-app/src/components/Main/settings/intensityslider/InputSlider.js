9ca0996f7b9e5ffb5db67f0b8b3b3a7c77c9293a
Christian-Mogensen <U2407192@rts-365.dk>
2022-02-02 18:18:16 +0100

    Add alpha logic to slider and lamp

--- demo/light-app/src/components/Main/settings/intensityslider/InputSlider.js
+++ demo/light-app/src/components/Main/settings/intensityslider/InputSlider.js
@@ -1,121 +1,136 @@
-import React, { useContext } from "react";
+import React from "react";
 import { getTrackBackground, Range } from "react-range";
 import Solution from "../../../../assets/img/icons/intensityslider/Solution";
 import Solution2 from "../../../../assets/img/icons/intensityslider/Solution2";
+import DemonstrationLight from "../../../../assets/img/icons/lights/DemonstrationLight";
 import { ColorContext } from "../../../../hooks/colorContext";
 import IntensityMarks from "./IntensityMarks";
+import { motion } from "framer-motion";
 
 const STEP = 1;
 const MIN = 0;
 const MAX = 99;
 
 class InputSlider extends React.Component {
-  static contextType = ColorContext
-
-  constructor(props){
-    super(props)
-    this.setState({alphaValues:
-      this.state.values < 10?
-      '00':
-      this.state.values < 90?
-      Math.ceil(this.state.values)
-      :''})
-  }
-
-  state = {
-    values: [0]
+  static contextType = ColorContext;
+  setAlphaValue = (alphaValue) => {
+    this.setState({ alphaValue });
   };
-  render() {
-    console.log(this.context);
-    return (
-        <div className="flex items-end w-full gap-3">
-<div><Solution /></div>
-      <div className="flex flex-col w-full gap-3 px-2"
+  state = {
+    values: [0],
+    alphaValue: this.context.alphaValue,
+    setAlphaValue: this.setAlphaValue,
+  };
 
+  render() {
+    return (
+      <div className="flex items-end w-full gap-3">
+        <motion.div
+          className="fixed top-0 left-[228px]"
+          initial={{ y: "-20%" }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.5, delay: 0.7 }}
         >
-        <Range
-          values={this.state.values}
-          step={STEP}
-          min={MIN}
-          max={MAX}
-          onChange={(values) =>( this.setState({ values }))}
-          renderTrack={({ props, children }) => (
-            <div
-              onMouseDown={props.onMouseDown}
-              onTouchStart={props.onTouchStart}
-              style={{
-                ...props.style,
-                display: "flex",
-                width: "100%",
-              }}
-            >
+          <DemonstrationLight
+            alpha={
+              this.state.values < 10
+                ? "10"
+                : this.state.values < 90
+                ? Math.ceil(this.state.values)
+                : ""
+            }
+          />
+        </motion.div>
+        <div>
+          <Solution />
+        </div>
+        <div className="flex flex-col w-full gap-3 px-2">
+          <Range
+            values={this.state.values}
+            step={STEP}
+            min={MIN}
+            max={MAX}
+            onChange={(values) =>
+              this.setState({ values }, this.setAlphaValue({ values }))
+            }
+            renderTrack={({ props, children }) => (
               <div
-                ref={props.ref}
+                onMouseDown={props.onMouseDown}
+                onTouchStart={props.onTouchStart}
                 style={{
-                  height: "2px",
+                  ...props.style,
+                  display: "flex",
                   width: "100%",
-                  borderRadius: "4px",
-                  background: getTrackBackground({
-                      values: this.state.values,
-                    colors: [`${this.context.colorTheme}`, "#ccc"],
-                    min: MIN,
-                    max: MAX
-                }),
-                  alignSelf: "center"
                 }}
+              >
+                <div
+                  ref={props.ref}
+                  style={{
+                    height: "2px",
+                    width: "100%",
+                    borderRadius: "4px",
+                    background: getTrackBackground({
+                      values: this.state.values,
+                      colors: [`${this.context.colorTheme}`, "#ccc"],
+                      min: MIN,
+                      max: MAX,
+                    }),
+                    alignSelf: "center",
+                  }}
                 >
-                {children}
+                  {children}
+                </div>
               </div>
-            </div>
-          )}
-          renderThumb={({ props, isDragged }) => (
+            )}
+            renderThumb={({ props, isDragged }) => (
               <div
-              {...props}
-              style={{
+                {...props}
+                style={{
                   ...props.style,
                   height: "15px",
-                width: "15px",
+                  width: "15px",
 
-                borderRadius: "9999px",
-                backgroundColor: "#FFF",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                boxShadow: `0px 2px 6px ${this.context.colorTheme}`
-            }}
-            >
-              <div
-                style={{
-                  height: "2px",
-                  width: "2px",
-                  backgroundColor: isDragged ? `${this.context.colorTheme}` : "#CCC"
+                  borderRadius: "9999px",
+                  backgroundColor: "#FFF",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  boxShadow: `0px 2px 6px ${this.context.colorTheme}`,
                 }}
+              >
+                <div
+                  style={{
+                    height: "2px",
+                    width: "2px",
+                    backgroundColor: isDragged
+                      ? `${this.context.colorTheme}`
+                      : "#CCC",
+                  }}
                 />
-            </div>
-          )}
-        />
-<IntensityMarks />
+              </div>
+            )}
+          />
+          <IntensityMarks />
+        </div>
+        <div className="relative">
+          <Solution2 clr="#cecece" />
+          <div className={`z-10 absolute top-0 left-0 right-0 bottom-0`}>
+            <Solution2
+              clr={`${this.context.colorTheme}${
+                this.state.values < 10
+                  ? "00"
+                  : this.state.values < 90
+                  ? Math.ceil(this.state.values)
+                  : ""
+              }`}
+            />
+          </div>
+        </div>
       </div>
-      <div className="relative"><Solution2 clr="#cecece" />
-      <div className={`z-10 absolute top-0 left-0 right-0 bottom-0`}><Solution2 clr={`${this.context.colorTheme}${this.state.values < 10?
-              '00':
-              this.state.values < 90?
-              Math.ceil(this.state.values)
-              :''}`} /></div>
-</div></div>
     );
   }
 }
-export default InputSlider
-
-// const InputSlider = () => {
-//   const { alphaValue, setAlphaValue, colorTheme } = useContext(ColorContext);
-//   console.log(alphaValue);
-
-
-
-  
-
+export default InputSlider;
 
 //   return (
 //     <div className="flex items-end w-full gap-3">
@@ -129,7 +144,7 @@ export default InputSlider
 //         step={1} value={alphaValue} onChange={(e)=>setAlphaValue(+e.target.value)} />
 //         <IntensityMarks />
 //       </div>
-      
+
 //       <div className="relative">
 //       <Solution2 clr="#cecece" />
 //       <div className={`z-10 absolute top-0 left-0 right-0 bottom-0`}>
