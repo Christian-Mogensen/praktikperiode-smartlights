1d3aeb32e8c47573865dbd89f6e12dc45cdd2015
Christian-Mogensen <U2407192@rts-365.dk>
2022-02-03 09:58:15 +0100

    Add solution 19

--- demo/light-app/src/components/Main/buttons/Button.js
+++ demo/light-app/src/components/Main/buttons/Button.js
@@ -10,19 +10,23 @@ const btnItem = {
   hidden: { opacity: 0, x:30 },
   show: { opacity: 1, x:0 }
 }
-const Button = ({clickRoute, variant, btnImg, btnH, btnS }) => {
-  const {colorTheme} = useContext(ColorContext)
+const Button = ({clickRoute, variant, btnImg, name, place, light }) => {
+  const {colorTheme, setRoomStartWord, setRoomEndWord, setLightHeader} = useContext(ColorContext)
   return (
-    <Link to={clickRoute}>
+    <Link to={clickRoute} onClick={()=>{
+      setRoomStartWord(name)
+      setRoomEndWord(place)
+      setLightHeader(light)
+    }}>
     <motion.div 
     variants={variant}
     className="bg-white cursor-pointer h-[150px] place-self-center rounded-[20px] w-[150px] p-5 btn__dropshadow-blue flex flex-col justify-between">
       <motion.div variants={btnItem}>
-        <motion.img variants={btnItem} src={`${btnImg}`} alt="" />
+        <motion.img variants={btnItem} src={`${btnImg}`} alt={`${name}${name} icon`} />
       </motion.div>
       <div>
-        <motion.span variants={btnItem} className="block text-lg font-bold">{btnH}</motion.span>
-        <motion.div variants={btnItem} style={{"color":`${colorTheme}`}}className={`font-bold text-[13px]`}>{btnS}</motion.div>
+        <motion.span variants={btnItem} className="block text-lg font-bold">{name}{place}</motion.span>
+        <motion.div variants={btnItem} style={{"color":`${colorTheme}`}}className={`font-bold text-[13px]`}>{light}</motion.div>
       </div>
     </motion.div>
       </Link>
