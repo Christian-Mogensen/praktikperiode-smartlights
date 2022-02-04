639d639c51b5212700d4b6c89bcfe338f977bc05
Christian-Mogensen <U2407192@rts-365.dk>
2022-02-04 08:58:00 +0100

    Add solution to issue 11

--- demo/light-app/src/components/Main/settings/scenes/SceneButton.js
+++ demo/light-app/src/components/Main/settings/scenes/SceneButton.js
@@ -2,14 +2,14 @@ import React, {useContext} from 'react';
 import SceneBulb from '../../../../assets/img/icons/scenesection/SceneBulb';
 import {motion} from 'framer-motion'
 import { ColorContext } from '../../../../hooks/colorContext';
-const SceneButton = ({fmiv,clr,val,keyframe}) => {
+const SceneButton = ({fmiv,val,keyframe}) => {
   const {setScene} = useContext(ColorContext)
   return <motion.div 
   onClick={()=>{setScene(keyframe)}}
   initial={{x:fmiv}}
   animate={{x:0}}
   transition={{duration:0.7, delay:1}}
-  className={`h-14 ${clr} cursor-pointer rounded-[10px] text-white grid button-column gap-3 items-center`}> <div className="w-full h-full flex justify-end items-center "><SceneBulb /></div><span>{val}</span></motion.div>;
+  className={`h-14 gradient${val} cursor-pointer rounded-[10px] text-white grid button-column gap-3 items-center`}> <div className="w-full h-full flex justify-end items-center "><SceneBulb /></div><span>{val}</span></motion.div>;
 };
 
 export default SceneButton;
