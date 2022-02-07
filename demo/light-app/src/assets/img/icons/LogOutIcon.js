a2cf5b00501bdc330e59682c2f3bc0040af22b0d
Christian-Mogensen <U2407192@rts-365.dk>
2022-02-07 12:07:22 +0100

    Add solution to issue 30

---
+++ demo/light-app/src/assets/img/icons/LogOutIcon.js
@@ -0,0 +1,30 @@
+import * as React from "react"
+
+const LogOutIcon = (props) => (
+  <svg
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    x={0}
+    y={0}
+    viewBox="0 0 56 56"
+    style={{
+      enableBackground: "new 0 0 56 56",
+    }}
+    xmlSpace="preserve"
+    {...props}
+  >
+    <style>
+      {".st0{fill:none;stroke:#000;stroke-width:4;stroke-miterlimit:10}"}
+    </style>
+    <path
+      className="st0"
+      d="M7.98 10.66C12.86 5 20.08 1.41 28.14 1.41c14.68 0 26.59 11.91 26.59 26.59S42.82 54.59 28.14 54.59c-8.05 0-15.27-3.58-20.14-9.23"
+    />
+    <path
+      className="st0"
+      d="M14.62 13.99 2.28 27.76l12.34 14.25M34.36 28H2.41"
+    />
+  </svg>
+)
+
+export default LogOutIcon
--- demo/light-app/src/components/Main/buttons/Button.js
+++ demo/light-app/src/components/Main/buttons/Button.js
@@ -10,7 +10,7 @@ const btnItem = {
   hidden: { opacity: 0, x:30 },
   show: { opacity: 1, x:0 }
 }
-const Button = ({clickRoute, variant, btnImg, name, place, light }) => {
+const Button = ({clickRoute, id, variant, btnImg, name, place, light }) => {
   
   
   
@@ -25,13 +25,14 @@ const Button = ({clickRoute, variant, btnImg, name, place, light }) => {
       setLightHeader(light)
     }}>
     <motion.div 
+    id={id}
     variants={variant}
     className="bg-white cursor-pointer h-[150px] place-self-center rounded-[20px] w-[150px] p-5 btn__dropshadow-blue flex flex-col justify-between">
       <motion.div variants={btnItem}>
         <motion.img variants={btnItem} src={`${btnImg}`} alt={`${name}${name} icon`} />
       </motion.div>
       <div className="w-full ">
-        <motion.div variants={btnItem} className="w-full text-lg font-bold flex gap-1"><div>{name}</div><div>{place}</div></motion.div>
+        <motion.div variants={btnItem} className="w-full text-lg font-bold flex gap-1 capitalize"><div>{name}</div><div>{place}</div></motion.div>
         <motion.div variants={btnItem} style={{"color":`${colorTheme}`}}className={`font-bold text-[13px]`}>{light}</motion.div>
       </div>
     </motion.div>
