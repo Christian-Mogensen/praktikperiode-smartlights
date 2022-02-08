1998a74de0882bffb8b883929ffeb1e18247eba7
Christian-Mogensen <U2407192@rts-365.dk>
2022-02-08 11:40:45 +0100

    Add firestore fetch logic and color picker system

--- demo/light-app/src/components/Main/settings/SettingWrapper.js
+++ demo/light-app/src/components/Main/settings/SettingWrapper.js
@@ -1,9 +1,6 @@
-import { doc, setDoc } from "firebase/firestore";
-import { motion } from "framer-motion";
-import React from "react";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Link, useParams } from "react-router-dom";
-import DemonstrationLight from "../../../assets/img/icons/lights/DemonstrationLight";
 import PowerOff from "../../../assets/img/icons/PowerOff";
 import { auth, db } from "../../../firebase/firebase";
 import { useColorContext } from "../../../hooks/colorContext";
@@ -11,24 +8,26 @@ import ColorOptions from "./colors/ColorOptions";
 import IntensitySlider from "./intensityslider/IntensitySlider";
 import SaveComp from "./SaveComp";
 import SceneContainer from "./scenes/SceneContainer";
+import { useEffect, useState } from "react";
 
 const SettingWrapper = () => {
   const {
     colorTheme,
     stateAlphaVal,
+    setColorTheme,
+    setStateAlphaVal,
+    setScene,
     scene,
     setRoomStartWord,
     setRoomEndWord,
     setLightHeader,
   } = useColorContext();
   const { slug } = useParams();
-  console.log(slug);
 
   const user = useAuthState(auth);
-  console.log();
 
+  const userId = user[0]?.uid;
   const createSetting = async (roomslug) => {
-    const userId = user[0].uid;
 
     await setDoc(doc(db, "users", userId, `rooms/${roomslug}`), {
       color: colorTheme,
@@ -36,7 +35,36 @@ const SettingWrapper = () => {
       scene: scene,
     });
   };
+  useEffect(()=>{
+    console.log(user[0]);
+  },[user])
 
+  useEffect(()=>{
+    (async()=>{
+
+      const docRef = doc(db, "users", userId, `rooms/${slug}`);
+      const docSnap = await getDoc(docRef);
+      const docData = docSnap.data()
+
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docData.brightness.x);
+      setStateAlphaVal({x:docData.brightness.x})
+      setColorTheme(docData.color)
+      setScene(docData.scene)
+      console.log(stateAlphaVal);
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    })()
+  },[])
+
+// const [isLoggedIn, setIsLoggedIn] = useState(false)
+// useEffect(()=>{
+//   return user && setIsLoggedIn(true)
+// },[user])
+// console.log(isLoggedIn);
   return (
     <div className="relative">
       <div className="absolute -right-2 -top-12">
@@ -51,9 +79,10 @@ const SettingWrapper = () => {
           <PowerOff />
         </Link>
       </div>
-      <div className=" -top-40   absolute" onClick={() => createSetting(slug)}>
+      
+      {userId&&<div className="absolute -top-40" onClick={() => createSetting(slug)}>
         <SaveComp />{" "}
-      </div>
+      </div>}
       <IntensitySlider />
       <ColorOptions />
       <SceneContainer />
--- demo/light-app/src/components/Main/settings/colors/ColorOptions.js
+++ demo/light-app/src/components/Main/settings/colors/ColorOptions.js
@@ -1,28 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../Header";
 import ColorCircle from "./ColorCircle";
 import { motion } from "framer-motion";
+import ColorPicker from "@vtaits/react-color-picker";
+import SaveComp from "../SaveComp";
+import LoginCloseIcon from '../../../../assets/img/icons/LoginCloseIcon'
+import { AiFillCloseCircle } from "react-icons/ai";
+import { GiSaveArrow } from "react-icons/gi";
+
+
+const COLOR = "#fa56cf";
 
 const ColorOptions = () => {
-  const colors = [
-    { clr: "#FF9B9B", val: "", fmiv:0 },
-    { clr: "#94EB9E", val: "", fmiv:-23 },
-    { clr: "#94CAEB", val: "", fmiv:-45 },
-    { clr: "#A594EB", val: "", fmiv:-65 },
-    { clr: "#DE94EB", val: "", fmiv:-87 },
-    { clr: "#EBD094", val: "", fmiv:-107 },
-    { clr: "#FFFFFF", val: "+", fmiv:-130 }
+  const [toggled, setToggled] = useState(false);
+  
+  
+  const colorArr = [
+    { clr: "#FF9B9B", val: "", fmiv: 0 },
+    { clr: "#94EB9E", val: "", fmiv: -23 },
+    { clr: "#94CAEB", val: "", fmiv: -45 },
+    { clr: "#A594EB", val: "", fmiv: -65 },
+    { clr: "#DE94EB", val: "", fmiv: -87 },
+    { clr: "#EBD094", val: "", fmiv: -107 },
   ];
+  const [colorPick, setColorPick] = useState(colorArr)
+  const handleAdd = () => {
+    // const newColorPick = colorPick.slice();
+    // newColorPick.push({clr:colorVal});
+    // setColorPick(newColorPick);
+  
+  setColorPick ((prevColorPick)=> [...prevColorPick, {clr:colorVal}]) 
+  
+  }
+
+  const [colorVal, setColorVal] = useState(COLOR);
+  const onDrag = (newColor) => {
+    setColorVal(newColor);
+  };
+
   return (
-    <div>
-      <Header value="Colors" />
-      <motion.div
-      className="flex gap-4 pb-6">
-        {colors.map((c, index) => (
-          <ColorCircle key={index} clr={c.clr} val={c.val} fm={c.fmiv} />
-        ))}
-      </motion.div>
-    </div>
+    <>
+      <div>
+        <Header value="Colors" />
+        <motion.div className="grid grid-cols-8 gap-4 pb-6">
+          {colorPick.map((c, index) => (
+            <ColorCircle key={index} clr={c.clr} val={c.val} fm={c.fmiv} />
+          ))}
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.7, delay: 1 }}
+            onClick={() => setToggled(!toggled)}
+            className={`relative cursor-pointer bg-white min-h-7 min-w-7 w-7 h-7 rounded-full grid place-content-center font-bold`}
+          >
+            +
+          </motion.div>
+          {toggled && (
+            <div className="fixed top-0 bottom-0 left-0 right-0">
+              {" "}
+              <ColorPicker className='p-8'
+              // saturationWidth={400} saturationHeight={500} hueWidth={100} 
+              value={colorVal} onDrag={onDrag} />
+              <div className="flex items-end gap-5">
+                <div
+                  style={{
+                    background: colorVal,
+                    width: 100,
+                    height: 50,
+                    color: "white",
+                  }}
+                >
+                  {colorVal}
+                </div>
+                <div className="grid w-8 h-8 text-lg bg-white rounded-full shadow-md cursor-pointer shadow-gray-600 place-content-center" onClick={handleAdd}>
+                  <GiSaveArrow />
+                </div>
+                <div className="grid w-8 h-8 text-lg bg-white rounded-full shadow-md cursor-pointer shadow-gray-600 place-content-center" onClick={()=>setToggled(!toggled)}>
+                  <div><AiFillCloseCircle /></div>
+                </div>
+              </div>
+            </div>
+          )}
+        </motion.div>
+      </div>
+    </>
   );
 };
 
